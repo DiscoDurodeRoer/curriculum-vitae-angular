ee1400a7215417b4c0be809e0e6bea84ab53f6dc
DiscoDurodeRoer <30374642+DiscoDurodeRoer@users.noreply.github.com>
2018-12-01 10:58:52 +0100

    Habilidades

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DatosService } from '../../services/datos.service';
 
 @Component({
   selector: 'app-skills',
@@ -7,9 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsComponent implements OnInit {
 
-  constructor() { }
+  private skills: any[];
+
+  constructor(private dataService: DatosService) { }
 
   ngOnInit() {
+
+    this.dataService.getData().subscribe(data => {
+      this.skills = data.json().habilidades;
+      
+      for(let skill of this.skills){
+        skill.class_color = this.randomColor();
+      }
+      console.log(this.skills);
+    }
+    );
+
   }
+
+  randomColor() {
+
+    let rand = Math.floor((Math.random() * 4) + 1);
+    let class_color = { "color": '', "class_progress": '' };
+    switch (rand) {
+      case 1:
+        class_color.color = "red";
+        class_color.class_progress = "progress-bar-danger";
+        break;
+      case 2:
+        class_color.color = "blue";
+        class_color.class_progress = "progress-bar-info";
+        break;
+      case 3:
+        class_color.color = "yellow";
+        class_color.class_progress = "progress-bar-warning";
+        break;
+      case 4:
+        class_color.color = "green";
+        class_color.class_progress = "progress-bar-success";
+        break;
+    }
+    return class_color;
+  }
+
 
 }
