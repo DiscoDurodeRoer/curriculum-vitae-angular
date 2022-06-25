092539fbda5716158a220cc72dc11f9a3172e188
DiscoDurodeRoer <30374642+DiscoDurodeRoer@users.noreply.github.com>
2022-06-25 12:15:57 +0200

    upload angular 13

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -46,13 +46,6 @@
 // import 'core-js/es7/reflect';
 
 
-/**
- * Required to support Web Animations `@angular/platform-browser/animations`.
- * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
- **/
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
-
-
 
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
